0656c321fd320c456f4e294c3b5c028a19d6c442
Matthew Yates <Mat.C.Yates@gmail.com>
2019-10-17 10:21:19 -0400

    .

--- api/models/Image.py
+++ api/models/Image.py
@@ -1,5 +1,17 @@
 from django.db import models
+from .User import User
 
 
 class Image(models.Model):
     url = models.URLField()
+    secure_url = models.URLField()
+    title = models.CharField()
+    height = models.CharField()
+    width = models.CharField()
+    imgFormat = models.CharField(verbose_name="format")
+    nsfw = models.BooleanField(default="false")
+    views = models.IntegerField()
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+    uploader = models.ForeignKey("User", on_delete=models.CASCADE)
+
