41c208d5753aff8ee5c225b5e80fb97632f52892
Matthew Yates <Mat.C.Yates@gmail.com>
2019-11-05 13:46:07 -0500

    migrating to fastapi

--- Dockerfile
+++
@@ -1,9 +0,0 @@
-FROM python:3.7
-
-WORKDIR /app
-
-RUN pip install pipenv
-COPY Pipfile Pipfile.lock .env ./
-RUN pipenv install --system
-
-COPY ./ ./
--- api/urls.py
+++
@@ -1,5 +0,0 @@
-from django.urls import path
-from rest_framework.authtoken import views as drf_views
-
-urlpatterns = [path("auth/", drf_views.ObtainAuthToken, name="auth")]
-
