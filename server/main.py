86321954f4b40e5266f8ad4fbf310cbf24df465b
Matthew Yates <Mat.C.Yates@gmail.com>
2019-11-06 15:26:43 -0500

    initial image endpoint scaffolding

--- server/main.py
+++ server/main.py
@@ -7,6 +7,12 @@ app = FastAPI()
 async def root():
     return {"message": "Hello world!"}
 
+
 @app.get("/images")
 async def get_all_images():
-    return {"images": [{"url": 'https://www.image.net'}]}
+    return {"images": [{"url": "https://www.image.net"}]}
+
+
+@app.get("/images/uploads/{user_id}")
+async def read_item(skip: int = 0, limit: int = 10, page: int = 1):
+    return {"images": [{"url": "foobar.com"}]}
